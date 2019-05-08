caea3c4ac3d77a8dad0c051bbb33eda9e8003eac
Nick Spiel <nick.spiel@rea-group.com>
2019-05-09 08:54:40 +1000

    Added global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,35 @@
-import React, { useState } from "react"
+import React, { useState } from 'react'
+import { createGlobalStyle } from 'styled-components'
 
-import SEO from "../components/seo"
+import SEO from '../components/seo'
 import SearchBar from '../components/SearchBar'
 import ResultsList from '../components/ResultsList'
-import mixpanel from '../services/mixpanel';
+import mixpanel from '../services/mixpanel'
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    margin: 0;
+    background-color: black;
+    color: white;
+    font-size: 16px;
+    text-align: center;
+    font-family: sans-serif;
+    padding: 2vw;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
 
 const IndexPage = () => {
   const [query, setQuery] = useState('')
@@ -13,6 +39,7 @@ const IndexPage = () => {
 
   return (
     <div>
+      <GlobalStyles />
       <SEO title="Search" />
       <SearchBar
         query={query}
